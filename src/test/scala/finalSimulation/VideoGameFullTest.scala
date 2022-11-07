7dc4c808939e156033d2f6e0fb6519c1681f2d8a
jwk71 <78984624+jwk71@users.noreply.github.com>
2022-11-07 22:20:30 +0100

    Update VideoGameFullTest.scala

--- src/test/scala/finalSimulation/VideoGameFullTest.scala
+++ src/test/scala/finalSimulation/VideoGameFullTest.scala
@@ -3,112 +3,85 @@ package finalSimulation
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import scala.concurrent.duration._
-import scala.util.Random
-
 class VideoGameFullTest extends Simulation {
 
-  val httpConf = http
-    .baseUrl("http://video-game-db.eu-west-2.elasticbeanstalk.com/app/")
-    .header("Accept", "application/json")
+  val httpProtocol = http.baseUrl("https://videogamedb.uk/api")
+    .acceptHeader("application/json")
+    .contentTypeHeader("application/json")
 
-  /*** Variables ***/
+  // ** VARIABLES FOR FEEDERS ** /
   // runtime variables
-  def userCount: Int = getProperty("USERS", "3").toInt
-  def rampDuration: Int = getProperty("RAMP_DURATION", "10").toInt
-  def testDuration: Int = getProperty("DURATION", "60").toInt
+  def USERCOUNT = System.getProperty("USERS", "5").toInt
+  def RAMPDURATION = System.getProperty("RAMP_DURATION", "10").toInt
+  def TESTDURATION: Int = System.getProperty("TEST_DURATION", "30").toInt
 
-  // other variables
-  var idNumbers = (20 to 1000).iterator
-  val rnd = new Random()
-  val now = LocalDate.now()
-  val pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+  val csvFeeder = csv("data/gameCsvFile.csv").random
 
-  /*** Helper Methods ***/
-  private def getProperty(propertyName: String, defaultValue: String) = {
-    Option(System.getenv(propertyName))
-      .orElse(Option(System.getProperty(propertyName)))
-      .getOrElse(defaultValue)
-  }
-
-  def randomString(length: Int) = {
-    rnd.alphanumeric.filter(_.isLetter).take(length).mkString
-  }
-
-  def getRandomDate(startDate: LocalDate, random: Random): String = {
-    startDate.minusDays(random.nextInt(30)).format(pattern)
-  }
-
-  /*** Custom Feeder ***/
-  val customFeeder = Iterator.continually(Map(
-    "gameId" -> idNumbers.next(),
-    "name" -> ("Game-" + randomString(5)),
-    "releaseDate" -> getRandomDate(now, rnd),
-    "reviewScore" -> rnd.nextInt(100),
-    "category" -> ("Category-" + randomString(6)),
-    "rating" -> ("Rating-" + randomString(4))
-  ))
-
-  /*** Before ***/
   before {
-    println(s"Running test with ${userCount} users")
-    println(s"Ramping users over ${rampDuration} seconds")
-    println(s"Total Test duration: ${testDuration} seconds")
+    println(s"Running test with ${USERCOUNT} users")
+    println(s"Ramping users over ${RAMPDURATION} seconds")
+    println(s"Total test duration: ${TESTDURATION} seconds")
   }
 
-  /*** HTTP Calls ***/
+  /*** HTTP CALLS ***/
   def getAllVideoGames() = {
     exec(
-      http("Get All Video Games")
-        .get("videogames")
-        .check(status.is(200)))
+      http("Get all video games")
+        .get("/videogame")
+        .check(status.is(200))
+    )
   }
 
-  def postNewGame() = {
-    feed(customFeeder).
-      exec(http("Post New Game")
-        .post("videogames")
-        .body(ElFileBody("bodies/NewGameTemplate.json")).asJson //template file goes in gating/resources/bodies
-        .check(status.is(200)))
+  def authenticate() = {
+    exec(http("Authenticate")
+      .post("/authenticate")
+      .body(StringBody("{\n  \"password\": \"admin\",\n  \"username\": \"admin\"\n}"))
+      .check(jsonPath("$.token").saveAs("jwtToken")))
   }
 
-  def getLastPostedGame() = {
-    exec(http("Get Last Posted Game")
-      .get("videogames/${gameId}")
-      .check(jsonPath("$.name").is("${name}"))
-      .check(status.is(200)))
+  def createNewGame() = {
+    feed(csvFeeder)
+      .exec(http("Create New Game - #{name}")
+        .post("/videogame")
+        .header("authorization", "Bearer #{jwtToken}")
+        .body(ElFileBody("bodies/newGameTemplate.json")).asJson)
   }
 
-  def deleteLastPostedGame() = {
-    exec(http("Delete Last Posted Game")
-      .delete("videogames/${gameId}")
-      .check(status.is(200)))
+  def getSingleGame() = {
+    exec(http("Get single game - #{name}")
+      .get("/videogame/#{gameId}")
+      .check(jsonPath("$.name").is("#{name}")))
   }
 
-  /*** Scenario Design ***/
-  val scn = scenario("Video Game DB")
-    .forever() {
+  def deleteGame() = {
+    exec(http("Delete game - #{name}")
+      .delete("/videogame/#{gameId}")
+      .header("authorization", "Bearer #{jwtToken}")
+      .check(bodyString.is("Video game deleted")))
+  }
+
+
+  /** SCENARIO DESIGN */
+  val scn = scenario("Video Game DB Final Script")
+    .forever {
       exec(getAllVideoGames())
         .pause(2)
-        .exec(postNewGame())
+        .exec(authenticate())
         .pause(2)
-        .exec(getLastPostedGame())
+        .exec(createNewGame())
         .pause(2)
-        .exec(deleteLastPostedGame())
+        .exec(getSingleGame())
+        .pause(2)
+        .exec(deleteGame())
     }
 
-  /*** Setup Load Simulation ***/
   setUp(
     scn.inject(
-      nothingFor(5.seconds),
-      rampUsers(userCount) during (rampDuration.seconds))
-  )
-    .protocols(httpConf)
-    .maxDuration(testDuration.seconds)
+      nothingFor(5),
+      rampUsers(USERCOUNT).during(RAMPDURATION)
+    ).protocols(httpProtocol)
+  ).maxDuration(TESTDURATION)
 
-  /*** After ***/
   after {
     println("Stress test completed")
   }
